8d28824c50474f496084f61dd193b0c5df1293de
Daisuke Sato <tiryoh@gmail.com>
2021-02-01 03:12:46 +0900

    feat: Support for firmware published on GitHub releases

--- scripts/00-clone-source.sh
+++ scripts/00-clone-source.sh
@@ -3,4 +3,6 @@ set -eu
 
 SRC_DIR=$(cd $(dirname ${BASH_SOURCE:-$0})/../; pwd)
 
-git clone https://github.com/elephantrobotics/myCobot.git $SRC_DIR/elephantrobotics-mycobot
+cd $SRC_DIR
+wget https://github.com/elephantrobotics/myCobot/releases/download/0/myCobot.V1.3.zip
+unzip myCobot.V1.3.zip
